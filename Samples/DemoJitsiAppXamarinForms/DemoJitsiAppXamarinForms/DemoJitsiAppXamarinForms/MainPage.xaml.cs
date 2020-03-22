02403c0754f7f4d5a4b4b58581338ee886c0ac33
Seesi <seesi@accedegh.net>
2020-03-22 03:53:10 +0000

    created xamarin forms project

---
+++ Samples/DemoJitsiAppXamarinForms/DemoJitsiAppXamarinForms/DemoJitsiAppXamarinForms/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoJitsiAppXamarinForms
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Skip)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }               
+    }
+}
---
+++ Samples/DemoJitsiAppXamarinForms/JitsiMeetViews.Shared/MyJitsiMeetView.cs
@@ -0,0 +1,38 @@
+﻿using JitsiXamarinBinding;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JitsiMeetViews.Shared
+{
+    public class MyJitsiMeetView: JitsiMeetView
+    {
+
+    }
+    public class JitsiMeetViewPage : JitsiMeetView
+    {
+        JitsiMeetConferenceOptions _viewOptions;
+        Action<JitsiMeetConferenceOptionsBuilder> _builder;
+        public JitsiMeetViewPage(Action<JitsiMeetConferenceOptionsBuilder> builder)
+        {
+            _builder = builder;
+        }
+        public JitsiMeetConferenceOptions ViewOptions
+        {
+            get
+            {
+                if (_viewOptions == null)
+                {
+                    _viewOptions = JitsiMeetConferenceOptions.FromBuilder(_builder);
+                }
+                return _viewOptions;
+
+            }
+        }
+
+        public void Join()
+        {
+            Join(ViewOptions);
+        }
+    }
+}
